1bd141727ea2a5bbcd5ca018ca55c28435276752
Cheonghakim <cheongha.kim@cyberone.kr>
2023-03-08 12:47:04 +0900

    add: test api

--- src/api/user/v1/UserInfoAPI.js
+++ src/api/user/v1/UserInfoAPI.js
@@ -1,8 +1,21 @@
 import axios from "axios";
 import { queryString } from "@/utils/utils";
 
-export class UserInfoAPI {
+export default class UserInfoAPI {
   static getUserInfo() {
     return axios.get(`${process.env.V1}/user/info${queryString({})}`);
+  }
+
+  static getTestInfo() {
+    return new Promise((resolve, reject) => {
+      const timer = setTimeout(() => {
+        clearTimeout(timer);
+        resolve({
+          userName: "Simpson",
+          age: 31,
+          gender: "Male",
+        });
+      }, 100);
+    });
   }
 }
--- src/views/v1/Home/HomeView.vue
+++ src/views/v1/Home/HomeView.vue
@@ -1,15 +1,35 @@
 <script>
-import { getCurrentInstance } from "vue";
+import UserInfoAPI from "@/api/user/UserInfoAPI";
+import { getCurrentInstance, reactive } from "vue";
 
 export default {
   setup() {
     const instance = getCurrentInstance();
     const proxy = instance.proxy;
     proxy.$subject.$emit("error", "test!");
+    const userData = reactive({ data: {} });
+
+    const getUserInfo = async () => {
+      try {
+        const data = await UserInfoAPI.v1.getTestInfo();
+        userData.data = data;
+      } catch (error) {
+        proxy.$subject.$emit("error", error);
+      }
+    };
+    getUserInfo();
+
+    return {
+      userData,
+    };
   },
 };
 </script>
 
 <template>
-  <div class="text-danger">메인화면</div>
+  <div>
+    <div class="text-danger">메인화면</div>
+    <div>{{ userData.data.userName }}</div>
+    <div>{{ userData.data.age }}</div>
+  </div>
 </template>
